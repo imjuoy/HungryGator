c1bd0cd24b2c4da1f095c0be7349d21f44bbce70
imjuoy <bsubhadeep95@gmail.com>
2018-04-02 17:37:58 -0400

    Added individual restaurant parser

--- ZomatoDataExtraction.py
+++ ZomatoDataExtraction.py
@@ -5,4 +5,12 @@ headers = {'Content-Type': 'application/json', 'user-key': '592789033e4cf0e384b5
 
 Response = requests.get('https://developers.zomato.com/api/v2.1/search?entity_id = 306&entity_type = city' , headers=headers)
 
-print(Response.json())
+Response_Json = Response.json()
+
+Array_length = Response_Json['restaurants']
+
+print(len(Array_length))
+
+for i in range(len(Array_length)):
+
+	print(Response_Json['restaurants'][i]['restaurant']['name'])
