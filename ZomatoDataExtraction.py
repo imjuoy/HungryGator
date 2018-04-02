a7373a5e194a632d15aed6a592a986980910a2bd
imjuoy <bsubhadeep95@gmail.com>
2018-04-02 15:18:57 -0400

    Fixed a typo

--- ZomatoDataExtraction.py
+++ ZomatoDataExtraction.py
@@ -3,6 +3,6 @@ from pprint import pprint
 
 headers = {'Content-Type': 'application/json', 'user-key': '592789033e4cf0e384b54c1ec424c706'}
 
-Response = requests.get('https://developers.zomato.com/api/v2.1/search?entity_id = 306&entity_typei = city' , headers=headers)
+Response = requests.get('https://developers.zomato.com/api/v2.1/search?entity_id = 306&entity_type = city' , headers=headers)
 
 print(Response.json())
