f69f5bc9e7943c7339e6f6e20bf1f9701da2894e
imjuoy <bsubhadeep95@gmail.com>
2018-04-04 11:18:55 -0400

    Changes in the Zomato Data Extraction Script

--- ZomatoDataExtraction.py
+++ ZomatoDataExtraction.py
@@ -3,6 +3,8 @@ from pprint import pprint
 
 headers = {'Content-Type': 'application/json', 'user-key': '592789033e4cf0e384b54c1ec424c706'}
 
+offset = 0
+
 Response = requests.get('https://developers.zomato.com/api/v2.1/search?entity_id = 306&entity_type = city' , headers=headers)
 
 Response_Json = Response.json()
