eb446bd5d49d05d0fed8fe7dedf83cbb05080a8a
imjuoy <bsubhadeep95@gmail.com>
2018-04-05 01:07:32 -0400

    Added Offset and extracted Restaurant Data

--- ZomatoDataExtraction.py
+++ ZomatoDataExtraction.py
@@ -4,15 +4,12 @@ from pprint import pprint
 headers = {'Content-Type': 'application/json', 'user-key': '592789033e4cf0e384b54c1ec424c706'}
 
 offset = 0
-
-Response = requests.get('https://developers.zomato.com/api/v2.1/search?entity_id = 306&entity_type = city' , headers=headers)
-
-Response_Json = Response.json()
-
-Array_length = Response_Json['restaurants']
-
-print(len(Array_length))
-
-for i in range(len(Array_length)):
-
-	print(Response_Json['restaurants'][i]['restaurant']['name'])
+resultSet = []
+while offset < 100:
+	url = 'https://developers.zomato.com/api/v2.1/search?entity_id=306&entity_type=city&start='+str(offset)+'&count=20'
+	Response = requests.get(url , headers=headers)
+	offset += 20
+	Response_Json = Response.json()
+	Array_length = Response_Json['restaurants']
+	for i in range(len(Array_length)):
+		print(Response_Json['restaurants'][i]['restaurant']['name'])
