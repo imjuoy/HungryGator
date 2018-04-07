d655b6bb3dc406b19d098ed19f6ae8beae341bb9
imjuoy <bsubhadeep95@gmail.com>
2018-04-07 17:46:08 -0400

    Changes in TripAdvisor Data Cleaning code

--- TripAdvisor_Data_Cleaning.py
+++ TripAdvisor_Data_Cleaning.py
@@ -1,12 +1,6 @@
-##This file should read the data from CSV and then filter ratings, votes and names,etc of restaurants only based out of San Francisco.
-
 import pandas as pd
 
-counter = 0
-
 df = pd.read_csv('TripAdvisor_Data.csv')
-Country_Names = df.Country
-for i in Country_Names:
-	if 'United States' in i:
-		counter += 1
-print(counter)
+df_Miami_Data = df[(df["Country"]=="United States")& (df["City"]=="Miami")]
+df_result = df_Miami_Data[["Name","Ranking","Rating","Total Review"]]
+print(df_result)
