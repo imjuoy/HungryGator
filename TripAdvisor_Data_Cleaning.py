451fa1ffa8a8ebeb9e357cc7aa688eb6a23031c6
imjuoy <bsubhadeep95@gmail.com>
2018-04-07 06:39:53 -0400

    Added code for cleaning Trip Advisor dat

--- TripAdvisor_Data_Cleaning.py
+++ TripAdvisor_Data_Cleaning.py
@@ -1,3 +1,12 @@
 ##This file should read the data from CSV and then filter ratings, votes and names,etc of restaurants only based out of San Francisco.
 
+import pandas as pd
 
+counter = 0
+
+df = pd.read_csv('TripAdvisor_Data.csv')
+Country_Names = df.Country
+for i in Country_Names:
+	if 'United States' in i:
+		counter += 1
+print(counter)
