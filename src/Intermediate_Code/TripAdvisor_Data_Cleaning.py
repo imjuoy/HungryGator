f845db2d68f47bcabf831aefcaeed34bbf13fa1e
imjuoy <bsubhadeep95@gmail.com>
2018-05-07 15:14:25 -0400

    Final Commit

--- src/Intermediate_Code/TripAdvisor_Data_Cleaning.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('TripAdvisor_Data.csv')
-df_Miami_Data = df[(df["Country"]=="United States")& (df["City"]=="Miami")]
-df_result = df_Miami_Data[["Name","Ranking","Rating","Total Review"]]
-print(df_result)
